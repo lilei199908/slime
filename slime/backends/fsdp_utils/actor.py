4b401aafb33be1f32802bc656bcbca2297e12552
lilei <799812479@qq.com>
2026-01-08 02:23:08 +0000

    add fault torlance

--- slime/backends/fsdp_utils/actor.py
+++ slime/backends/fsdp_utils/actor.py
@@ -812,14 +812,15 @@ class FSDPTrainRayActor(TrainRayActor):
         if self.args.debug_train_only or self.args.debug_rollout_only:
             return
 
-        rollout_engines, rollout_engine_lock, num_new_engines = ray.get(
+        rollout_engines, rollout_engine_lock, _, need_connect_train_actors = ray.get(
             self.rollout_manager.get_rollout_engines_and_lock.remote()
         )
-        if num_new_engines > 0:
+
+        if need_connect_train_actors:
             self.weight_updater.connect_rollout_engines(rollout_engines, rollout_engine_lock)
             dist.barrier(group=get_gloo_group())
             if dist.get_rank() == 0:
-                ray.get(self.rollout_manager.clear_num_new_engines.remote())
+                ray.get(self.rollout_manager.set_need_connect_train_actors.remote(False))
 
         self.weight_updater.update_weights()
 
--- slime/backends/megatron_utils/actor.py
+++ slime/backends/megatron_utils/actor.py
@@ -514,18 +514,19 @@ class MegatronTrainRayActor(TrainRayActor):
                 ray.get(self.rollout_manager.recover_rollout_engines.remote())
             dist.barrier(group=get_gloo_group())
 
-        rollout_engines, rollout_engine_lock, num_new_engines = ray.get(
+        rollout_engines, rollout_engine_lock, _, need_connect_train_actors = ray.get(
             self.rollout_manager.get_rollout_engines_and_lock.remote()
         )
 
         if self.args.offload_train:
             reload_process_groups()
 
-        if num_new_engines > 0:
+        if need_connect_train_actors:
             self.weight_updater.connect_rollout_engines(rollout_engines, rollout_engine_lock)
             dist.barrier(group=get_gloo_group())
             if dist.get_rank() == 0:
-                ray.get(self.rollout_manager.clear_num_new_engines.remote())
+                print("Connected rollout engines to train actors.")
+                ray.get(self.rollout_manager.set_need_connect_train_actors.remote(False))
 
         with torch_memory_saver.disable() if self.args.offload_train else nullcontext():
             print_memory("before update_weights")
